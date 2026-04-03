536b728a0e62f759160e50480179fe61a48971e4
DucMinh1611 <125890449+Ducminh1611@users.noreply.github.com>
2026-04-04 06:59:52 +0700

    Update etl_load_staging.py

--- Python_ETL/etl_load_staging.py
+++ Python_ETL/etl_load_staging.py
@@ -2,10 +2,9 @@ import pandas as pd
 from sqlalchemy import create_engine
 import os
 
-# --- CẤU HÌNH (Sửa lại cho đúng máy bạn) ---
-SERVER_NAME = r'.\SQLEXPRESS'  # Nếu lỗi, thử: 'LOCALHOST\SQLEXPRESS' hoặc tên máy tính của bạn
+SERVER_NAME = r'.\SQLEXPRESS'  
 DATABASE_NAME = 'Ecommerce_BI_Project'
-DATA_FOLDER = r'D:\Data' # <--- SỬA ĐƯỜNG DẪN NÀY
+DATA_FOLDER = r'D:\Data'
 
 # Tạo kết nối tới SQL Server (Dùng Windows Authentication)
 connection_string = f"mssql+pyodbc://@{SERVER_NAME}/{DATABASE_NAME}?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
@@ -25,13 +24,13 @@ files_map = {
 }
 
 def load_data():
-    print("🚀 Bắt đầu nạp dữ liệu vào Staging...")
+    print(" Bắt đầu nạp dữ liệu vào Staging...")
     
     for csv_file, table_name in files_map.items():
         file_path = os.path.join(DATA_FOLDER, csv_file)
         
         if os.path.exists(file_path):
-            print(f"⏳ Đang xử lý: {table_name}...")
+            print(f" Đang xử lý: {table_name}...")
             try:
                 # Đọc CSV
                 df = pd.read_csv(file_path)
@@ -39,13 +38,13 @@ def load_data():
                 # Đẩy vào SQL Server (Schema 'staging', nếu bảng có rồi thì thay thế)
                 df.to_sql(table_name, engine, schema='staging', if_exists='replace', index=False)
                 
-                print(f"✅ Đã nạp xong bảng: staging.{table_name} ({len(df)} dòng)")
+                print(f" Đã nạp xong bảng: staging.{table_name} ({len(df)} dòng)")
             except Exception as e:
-                print(f"❌ Lỗi bảng {table_name}: {e}")
+                print(f"Lỗi bảng {table_name}: {e}")
         else:
-            print(f"⚠️ Không tìm thấy file: {csv_file}")
+            print(f" Không tìm thấy file: {csv_file}")
 
-    print("\n🎉 Hoàn tất quá trình ETL Staging!")
+    print("\n Hoàn tất quá trình ETL Staging!")
 
 if __name__ == "__main__":
     load_data()
